4f3b89fb7830887959c8774382ae51422e115752
Mikica Kocic <mikica.kocic@fysik.su.se>
2019-08-27 21:20:46 +0200

    Added MPI_Triangle and MPI_TrianglePacked datatypes

--- lab3/dtypes.cpp
+++ lab3/dtypes.cpp
@@ -1,12 +1,21 @@
 /**
-    # Exercise 1 - MPI I/O
+    Exercise 2 - MPI I/O and derived types
 
-    MPI I/O is used so that results can be written to the same file in parallel.
-    Take the serial hello world programs and modify them so that instead of
-    writing the output to screen the output is written to a file using MPI I/O.
+    Take the serial stl reader and modify it such that the stl file is read 
+    (and written) in parallel using collective MPI I/O. Use derived types such 
+    that the file can be read/written with a maximum of 3 I/O operations per 
+    read and write.
 
-    The simplest solution is likely to be for you to create a character buffer,
-    and then use the MPI_File_write_at function.
+    The simplest solution is likely to create a derived type for each triangle,
+    and then use the MPI_File_XXXX_at_all function. A correct solution will 
+    have the same MD5 hash for both stl models (input and output), unless 
+    the order of the triangles has been changed.
+
+    md5sum out.stl data/sphere.stl
+
+        822aba6dc20cc0421f92ad50df95464c  out.stl
+        822aba6dc20cc0421f92ad50df95464c  data/sphere.stl
+
 
     STL file format:
     ```
@@ -33,7 +42,7 @@
 
 #include <mpi.h>
 
-
+///////////////////////////////////////////////////////////////////////////////
 
 class STL_Model
 {
@@ -56,11 +65,27 @@ protected:
     Triangle*  tri;             //!< Triangles
     char       hdr[HDR_SIZE];   //!< Header
 
+    ///////////////////////////////////////////////////////////////////////////
+
+    static void debug( const char* prefix, MPI::Offset offset, size_t len )
+    {
+        std::cout << std::setw(2) << MPI::COMM_WORLD.Get_rank () 
+            << ": " << prefix << " "  \
+            << std::setw(7) << offset << " -- "  \
+            << std::setw(7) << offset + len - 1 \
+            << ", len = " << len << " octets" << std::endl;
+    }
+
 public:
 
     STL_Model ()
         : n_tri( 0 ), tri( NULL )
     {
+        if ( MPI::COMM_WORLD.Get_rank() == 0 ) {
+            std::cout << std::endl << "STL header size = " << HDR_SIZE 
+                << std::endl << "sizeof(Triangle) = " << sizeof(Triangle )
+                << std::endl;
+        }
     }
 
     ~STL_Model ()
@@ -72,18 +97,12 @@ public:
 
     virtual void read( const char* fname )
     {
-        int pe_size, pe_rank;
-        MPI_Comm_size( MPI_COMM_WORLD, &pe_size );
-        MPI_Comm_rank( MPI_COMM_WORLD, &pe_rank );
-
         FILE* fp = fopen( fname, "rb" );
 
+        int pe_rank = MPI::COMM_WORLD.Get_rank ();
         if( pe_rank == 0 ) {
             std::cout << std::endl 
-                << "Reading STL file: " << fname
-                << std::endl << "Header size =  " << HDR_SIZE 
-                << std::endl << "sizeof(Triangle) = " << sizeof(Triangle )
-                << std::endl;
+                << "Reading STL file: " << fname << std::endl;
         }
 
         // Read STL header
@@ -92,7 +111,7 @@ public:
 
         // Make sure it's a binary STL file with the correct header length
         //
-        if ( rc != HDR_SIZE || strncmp( hdr, "solid", 5) == 0)
+        if ( rc != HDR_SIZE || strncmp( hdr, "solid", 5 ) == 0)
         {
             std:: cerr << "ASCII STL files are not supported!" << std::endl;
             exit( -1 );
@@ -122,12 +141,9 @@ public:
 
     virtual void write( const char* fname )
     {
-        int pe_size, pe_rank;
-        MPI_Comm_size( MPI_COMM_WORLD, &pe_size );
-        MPI_Comm_rank( MPI_COMM_WORLD, &pe_rank );
-
         // Only the master process can write the complete file
         //
+        int pe_rank = MPI::COMM_WORLD.Get_rank ();
         if ( pe_rank != 0 ) 
         {  
             return;
@@ -156,80 +172,66 @@ public:
     }
 };
 
-class STL_Model_MPI : public STL_Model
-{
-    void debug_write( int pe_rank, MPI_Offset offset, size_t len )
-    {
-        std::cout << std::setw(2) << pe_rank << ": written "  \
-            << std::setw(7) << offset << " -- "  \
-            << std::setw(7) << offset + len - 1 \
-            << ", len = " << len << " octets" << std::endl;
-    }
+///////////////////////////////////////////////////////////////////////////////
 
+class STL_Model_MpiSimple : public STL_Model
+{
 public:
 
     virtual void write( const char* fname )
     {
-        int pe_size, pe_rank;
-        MPI_Comm_size( MPI_COMM_WORLD, &pe_size );
-        MPI_Comm_rank( MPI_COMM_WORLD, &pe_rank );
-
         // Open the file for writing
         //
-        MPI_File fp;
-        MPI_File_open( MPI_COMM_WORLD,
-                   fname, MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                   MPI_INFO_NULL, &fp );
+        MPI::File fp = MPI::File::Open( MPI::COMM_WORLD,
+                   fname, MPI::MODE_CREATE | MPI::MODE_WRONLY,
+                   MPI::INFO_NULL );
 
+        int pe_rank = MPI::COMM_WORLD.Get_rank ();
         if( pe_rank == 0 ) {
-            std::cout << std::endl << "MPI Writing STL file: " << fname << std::endl;
+            std::cout << std::endl << "MPI (Simple) Writing STL file: " 
+                << fname << std::endl;
         }
 
         // Write STL header
         //
-        // fwrite( hdr, 1, HDR_SIZE, fp );
-        //
-        MPI_Offset offset = 0;
+        MPI::Offset offset = 0;
         size_t len = HDR_SIZE;
         if( pe_rank == 0 ) 
         {
-            MPI_Status status;
-            MPI_File_write_at( fp, offset, hdr, len, MPI_CHAR, &status );
-            debug_write( pe_rank, offset, len );
+            MPI::Status status;
+            fp.Write_at( offset, hdr, len, MPI::BYTE, status );
+            debug( "write", offset, len );
         }
         offset += len;
 
         // Write number of triangles
         //
-        // fwrite( &n_tri, sizeof(uint32_t), 1, fp );
-        //
         len = sizeof(uint32_t);
         if( pe_rank == 0 ) 
         {
-            MPI_Status status;
-            MPI_File_write_at( fp, offset, &n_tri, len, MPI_CHAR, &status );
-            debug_write( pe_rank, offset, len );
+            MPI::Status status;
+            fp.Write_at( offset, &n_tri, len, MPI::BYTE, status );
+            debug( "write", offset, len );
         }
         offset += len;
 
-        MPI_Barrier( MPI_COMM_WORLD ); // Wait for the header to be written
+        MPI::COMM_WORLD.Barrier (); // Wait for the header to be written
 
         // Write all triangles
         //
-        // fwrite( tri, sizeof(Triangle), n_tri, fp );
-        //
+        int pe_size = MPI::COMM_WORLD.Get_size ();
         size_t chunk_size = n_tri / pe_size;
         offset += pe_rank * chunk_size * sizeof(Triangle);
         len = ( pe_rank < pe_size - 1 ? chunk_size : n_tri - pe_rank * chunk_size ) 
             * sizeof(Triangle);
 
-        MPI_Status status;
-        MPI_File_write_at_all( fp, offset, &tri[ pe_rank * chunk_size ], len, MPI_CHAR, &status );
-        debug_write( pe_rank, offset, len );
+        MPI::Status status;
+        fp.Write_at_all( offset, &tri[ pe_rank * chunk_size ], len, MPI::BYTE, status );
+        debug( "write", offset, len );
 
         // Close the file
         //
-        MPI_File_close( &fp );
+        fp.Close ();
 
         if( pe_rank == 0 ) {
             std::cout << "Done writing." << std::endl;
@@ -237,17 +239,226 @@ public:
     }
 };
 
+///////////////////////////////////////////////////////////////////////////////
+
+class STL_Model_MpiDT : public STL_Model
+{
+    MPI::Datatype MPI_Triangle;
+    MPI::Datatype MPI_TrianglePacked;
+
+public:
+
+    STL_Model_MpiDT ()
+    {
+        int blockcounts[2];
+        MPI::Aint offsets[2];
+        MPI::Datatype oldtypes[2];
+
+        // Setup description of the 4*3 MPI_FLOAT
+        // This block covers: 'n[3], v1[3], v2[3], v3[3]'
+        //
+        offsets[0] = 0;
+        oldtypes[0] = MPI::FLOAT;
+        blockcounts[0] = 4 * 3;
+
+        // Need to first figure offset by getting size of MPI_FLOAT
+        MPI::Aint lb, extent;
+        MPI::FLOAT.Get_extent( lb, extent );
+
+        // Setup description of the 1 MPI_INT field
+        // This block covers: 'attrib'
+        //
+        offsets[1] = 4 * 3 * extent;
+        oldtypes[1] = MPI::UNSIGNED_SHORT;
+        blockcounts[1] = 1;
+
+        // Define structured type and commit it
+        //
+        MPI_Triangle = MPI::Datatype::Create_struct( 2, blockcounts, offsets, oldtypes );
+        MPI_Triangle.Commit ();
+
+        // Since wi have packed structure, we need to reduce the extent
+        //
+        MPI_Triangle.Get_extent( lb, extent );
+        MPI_TrianglePacked = MPI_Triangle.Create_resized( lb, extent -2 );
+        MPI_TrianglePacked.Commit ();
+
+        // Report creation
+        //
+        if ( MPI::COMM_WORLD.Get_rank() == 0 ) {
+            std::cout << "MPI_Triangle extent = " << extent << std::endl;
+            MPI_TrianglePacked.Get_extent( lb, extent );
+            std::cout << "MPI_TrianglePacked extent = " << extent << std::endl;
+        }
+    }
+
+    virtual void read( const char* fname )
+    {
+        // Open the file for reading
+        //
+        MPI::File fp = MPI::File::Open( MPI::COMM_WORLD,
+                   fname, MPI::MODE_RDONLY,
+                   MPI::INFO_NULL );
+
+        int pe_rank = MPI::COMM_WORLD.Get_rank ();
+        if( pe_rank == 0 ) {
+            std::cout << std::endl 
+                << "MPI (DT) Reading STL file: " << fname << std::endl;
+        }
+
+        // Read STL header
+        //
+        MPI::Offset offset = 0;
+        size_t len = HDR_SIZE;
+        // if( pe_rank == 0 ) // everyone must read header
+        {
+            MPI::Status status;
+            fp.Read_at_all( offset, hdr, len, MPI::BYTE, status );
+            debug( "readH", offset, len );
+
+            // Make sure it's a binary STL file with the correct header length
+            //
+            if ( strncmp( hdr, "solid", 5 ) == 0)
+            {
+                std:: cerr << "ASCII STL files are not supported!" << std::endl;
+                exit( -1 );
+            }
+        }
+        offset += len;
+
+        // Read number of triangles
+        //
+        len = sizeof(uint32_t);
+        // if( pe_rank == 0 ) // everyone must read header
+        {
+            MPI::Status status;
+            fp.Read_at_all( offset, &n_tri, len, MPI::BYTE, status );
+            debug( "readN", offset, len );
+
+            // Allocate memory for triangles
+            //
+            tri = new Triangle[ n_tri ];
+        }
+        offset += len;
+
+        if( pe_rank == 0 ) {
+            std::cout << "Found: " << n_tri << " triangles" << std::endl;
+        }
+
+        // Read all triangles using MPI_TrianglePacked datatype
+        //
+        int pe_size = MPI::COMM_WORLD.Get_size ();
+        size_t chunk_size = n_tri / pe_size;
+        offset += pe_rank * chunk_size * sizeof(Triangle);
+        len = ( pe_rank < pe_size - 1 ? chunk_size : n_tri - pe_rank * chunk_size );
+
+        MPI::Status status;
+        fp.Read_at_all( offset, &tri[ pe_rank * chunk_size ], len, 
+                         MPI_TrianglePacked, status );
+        debug( "readD", offset, len * sizeof(Triangle) );
+
+        // Close the file
+        //
+        fp.Close ();
+
+        if( pe_rank == 0 ) {
+            std::cout << "Done reading." << std::endl;
+        }
+
+        // MPI::COMM_WORLD.Barrier (); // Wait for the header to be read
+    }
+
+    virtual void write( const char* fname )
+    {
+        // Open the file for writing
+        //
+        MPI::File fp = MPI::File::Open( MPI::COMM_WORLD,
+                   fname, MPI::MODE_CREATE | MPI::MODE_WRONLY,
+                   MPI::INFO_NULL );
+
+        int pe_rank = MPI::COMM_WORLD.Get_rank ();
+        if( pe_rank == 0 ) {
+            std::cout << std::endl << "MPI (DT) Writing STL file: "
+                << fname << std::endl;
+        }
+
+        // Write STL header
+        //
+        MPI::Offset offset = 0;
+        size_t len = HDR_SIZE;
+        if( pe_rank == 0 ) 
+        {
+            MPI::Status status;
+            fp.Write_at( offset, hdr, len, MPI::BYTE, status );
+            debug( "write", offset, len );
+        }
+        offset += len;
+
+        // Write number of triangles
+        //
+        len = sizeof(uint32_t);
+        if( pe_rank == 0 ) 
+        {
+            MPI::Status status;
+            fp.Write_at( offset, &n_tri, len, MPI::BYTE, status );
+            debug( "write", offset, len );
+        }
+        offset += len;
+
+        MPI::COMM_WORLD.Barrier (); // Wait for the header to be written
+
+        // Write all triangles using MPI_TrianglePacked datatype
+        //
+        int pe_size = MPI::COMM_WORLD.Get_size ();
+        size_t chunk_size = n_tri / pe_size;
+        offset += pe_rank * chunk_size * sizeof(Triangle);
+        len = ( pe_rank < pe_size - 1 ? chunk_size : n_tri - pe_rank * chunk_size );
+
+        MPI::Status status;
+        fp.Write_at_all( offset, &tri[ pe_rank * chunk_size ], len, 
+                         MPI_TrianglePacked, status );
+        debug( "write", offset, len * sizeof(Triangle) );
+
+        // Close the file
+        //
+        fp.Close ();
+
+        if( pe_rank == 0 ) {
+            std::cout << "Done writing." << std::endl;
+        }
+    }
+};
+
+
 int main( int argc, char** argv )
 {
-    MPI_Init( &argc, &argv );
+    const char* infname  = argc >= 2 ? argv[1] : "data/sphere.stl";
+    const char* outfname = argc >= 3 ? argv[2] : "out.stl";
+    int classType = argc >= 4 ? atoi( argv[3] ) : 2;
 
-    STL_Model_MPI model;
+    MPI::Init( argc, argv );
 
-    model.read ( argc >= 2 ? argv[1] : "data/sphere.stl" );
-    model.write( argc >= 3 ? argv[2] : "out.stl"         );
+    if( classType == 2 )
+    {
+        STL_Model_MpiDT model;
+        model.read( infname );
+        model.write( outfname );
+    }
+    else if ( classType == 1 )
+    {
+        STL_Model_MpiSimple model;
+        model.read( infname );
+        model.write( outfname );
+    }
+    else
+    {
+        STL_Model model;
+        model.read( infname );
+        model.write( outfname );
+    }
 
-    MPI_Barrier( MPI_COMM_WORLD );
-    MPI_Finalize ();
+    MPI::COMM_WORLD.Barrier ();
+    MPI::Finalize ();
 
     return 0;
 }
